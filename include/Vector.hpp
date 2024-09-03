0ccc361453388a8d9f3a3bf365d54e69caa12683
isai7710 <isai10sanchez@gmail.com>
2024-09-03 13:24:16 -0600

    started with dynamically allocating size of vector given user input and created unit tests to check that size was correctly allocated, next step is to test with Valgrind for memory leaks

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -1,37 +1,43 @@
-#ifndef VECTOR3D_HPP
-#define VECTOR3D_HPP
-#include <array>
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+#include <stddef.h>
 
-class Vector
-{
+class Vector {
 public:
-  Vector();
-  Vector(double x, double y, double z);
+  // Constructors
+  Vector(size_t size);
+  Vector(const Vector &other);     // Copy constructor
+  Vector(Vector &&other) noexcept; // Move constructor
 
-  const double& operator[](unsigned int i) const;//You need to add this so that you can use the [] in your const functions.
-  double& operator[](unsigned int i);
+  ~Vector();
 
+  // Assignment operators
+  Vector &operator=(const Vector &other);     // Copy assignment
+  Vector &operator=(Vector &&other) noexcept; // Move assignment
 
-  void multiply_by_scalar(double c);//These two should be * and / operators
+  // Access and modification
+  const double &operator[](size_t index) const;
+  double &operator[](size_t index);
+
+  // Utility methods
+  int size() const;
+
+  void multiply_by_scalar(double c); // These two should be * and / operators
   void divide_by_scalar(double c);
 
-  int size() const;
   double length() const;
   double length_squared() const;
 
   void normalize();
 
-  double get_x() const;
-  double get_y() const;
-  double get_z() const;
-
-protected:
-  std::array<double,3> components{0.0,0.0,0.0};
+private:
+  size_t size_;
+  double *data_;
 };
 
 Vector operator+(const Vector &v1, const Vector &v2);
 Vector operator-(const Vector &v1, const Vector &v2);
-Vector cross_product(const Vector& vector1, const Vector& vector2); //ASK ABOUT const and &
-double dot_product(const Vector& vector1, const Vector& vector2);
+Vector cross_product(const Vector &vector1, const Vector &vector2);
+double dot_product(const Vector &vector1, const Vector &vector2);
 
 #endif
---
+++ unit_tests/vector_unit_tests.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <Vector.hpp>
+
+class PreBuiltVector : public testing::Test {
+protected:
+// This is the vector instance that will be used in the tests
+    Vector vec;
+
+    // Constructor for the test fixture
+    PreBuiltVector() : vec(4) { }
+
+    // Destructor for the test fixture with override if necessary to override in future
+    ~PreBuiltVector() override { }
+};
+
+TEST_F(PreBuiltVector, GivenAPreBuiltVector_WhenCheckingSize_) {
+  EXPECT_EQ(vec.size(), 4);
+}
