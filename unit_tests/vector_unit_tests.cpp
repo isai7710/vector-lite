362db92f0b20f30773404abd5993ccdc3444cd02
isai7710 <isai10sanchez@gmail.com>
2024-09-03 16:48:40 -0600

    adding unit tests

--- unit_tests/vector_unit_tests.cpp
+++ unit_tests/vector_unit_tests.cpp
@@ -1,18 +1,20 @@
-#include <gtest/gtest.h>
 #include <Vector.hpp>
+#include <gtest/gtest.h>
 
 class PreBuiltVector : public testing::Test {
 protected:
-// This is the vector instance that will be used in the tests
-    Vector vec;
+  // This is the vector instance that will be used in the tests
+  Vector vec;
 
-    // Constructor for the test fixture
-    PreBuiltVector() : vec(4) { }
+  // Constructor for the test fixture
+  PreBuiltVector() : vec(4) {}
 
-    // Destructor for the test fixture with override if necessary to override in future
-    ~PreBuiltVector() override { }
+  // Destructor for the test fixture with override if necessary to override in
+  // future
+  ~PreBuiltVector() override {}
 };
 
 TEST_F(PreBuiltVector, GivenAPreBuiltVector_WhenCheckingSize_) {
   EXPECT_EQ(vec.size(), 4);
 }
+
